f0051da3be8ec229f2da2e804f0268448ed4345c
omer tugra <omrtugraaltungok@gmail.com>
2025-05-26 23:46:59 +0300

    Düzenlendi

--- Java101/src/PratikBoksOyunu/Main.java
+++ Java101/src/PratikBoksOyunu/Main.java
@@ -3,7 +3,7 @@ package PratikBoksOyunu;
 public class Main {
     public static void main(String[] args) {
       Fighter marc = new Fighter("Marc" , 15 , 100, 90, 40);
-      Fighter alex = new Fighter("Alex" , 10 , 95, 100, 50);
+      Fighter alex = new Fighter("Alex" , 10 , 95, 100, 40);
       Match r = new Match(marc,alex , 90 , 100);
       r.run();
     }
